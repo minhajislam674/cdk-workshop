f094fc159698076fa06554906e5eeb0ac9e4db1d
Minhaj Islam <minhajislam674@gmail.com>
2023-09-23 23:05:21 +0200

    first commit

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,20 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const hello = new NodejsFunction(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_18_X, // execution environment
+      entry: "lambda/hello.ts", // file is "hello.ts"
+      handler: "index.handler", // file is "hello.ts" is bundeled into index.js, where the function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const api = new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
   }
 }
